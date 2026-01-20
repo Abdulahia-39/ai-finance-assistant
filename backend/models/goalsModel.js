13b755d7105cb20d2c0248ea477b6646bd46c0a0
Abdiaziz Mohamed Ahmed <updiaziiz45@gmail.com>
2026-01-20 10:46:35 +0300

    fixed the goals model

---
+++ backend/models/goalsModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const GoalSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true }, // e.g., "New MacBook"
+  targetAmount: { type: Number, required: true },
+  currentAmount: { type: Number, default: 0 },
+  category: { type: String, default: "General" },
+  deadline: { type: Date },
+  status: { type: String, enum: ["active", "completed"], default: "active" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Goal = mongoose.model("Goal", GoalSchema);
+
+export default Goal;
